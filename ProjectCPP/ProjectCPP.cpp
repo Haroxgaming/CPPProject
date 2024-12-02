7c23fc9c062fc5eebc8a8f876e5d399d1a7e2f48
Haroxgaming <mathis.denamur.pro@gmail.com>
2024-12-02 13:23:29 +0100

    Init commit
    Init commit

---
+++ ProjectCPP/ProjectCPP.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+    std::cout<<"--------------Appuyer sur entrer pour commencer--------------"<<std::endl;
+    int key = _getch();
+    return 0;
+}
