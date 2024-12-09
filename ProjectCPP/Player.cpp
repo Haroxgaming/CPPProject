3e03b216f3f89c20e4e8579a2a1e1ef2a879d73e
Haroxgaming <mathis.denamur.pro@gmail.com>
2024-12-09 10:31:34 +0100

    Getter/setter player
    Getter/setter player

--- ProjectCPP/Player.cpp
+++ ProjectCPP/Player.cpp
@@ -1,7 +1,52 @@
 ﻿#include "Player.h"
 #include <iostream>
 
+void Player::setarmor(int armor)
+{
+    armure = armor <= 0 ? 5 : armor;
+}
+
 int Player::getarmor()
 {
     return armure;
+}
+
+void Player::setPointDeVie(int vie)
+{
+    pointsDeVie = vie <= 0 ? 5 : vie;
+}
+
+int Player::getPointDeVie()
+{
+    return pointsDeVie;
+}
+
+void Player::setAttaque(int attack)
+{
+    attaque = attack <= 0 ? 5 : attack;
+}
+
+int Player::getAttaque()
+{
+    return attaque;
+}
+
+void Player::setGold(int money)
+{
+    gold = money <= 0 ? 5 : money;
+}
+
+int Player::getGold()
+{
+    return gold;
+}
+
+void Player::setSpellNumber(int spell)
+{
+    spellNumber = spell <= 0 ? 5 : spell;
+}
+
+int Player::getSpellNumber()
+{
+    return spellNumber;
 }
--- ProjectCPP/Player.h
+++ ProjectCPP/Player.h
@@ -2,6 +2,7 @@
 #include <string>
 #include "ennemy.h"
 #include <iostream>
+
 class Player
 {
 private:
@@ -10,6 +11,8 @@ private:
     int attaque;
     int armure;
     int inventory [5];
+    int gold;
+    int spellNumber;
 
 public:
     
@@ -20,6 +23,20 @@ public:
 
 
     void attack(ennemy& target);
+
+    void setarmor(int armor);
     int getarmor();
+
+    void setPointDeVie(int vie);
+    int getPointDeVie();
+
+    void setAttaque(int attack);
+    int getAttaque();
+
+    void setGold(int money);
+    int getGold();
+
+    void setSpellNumber(int spell);
+    int getSpellNumber();
     
 };
