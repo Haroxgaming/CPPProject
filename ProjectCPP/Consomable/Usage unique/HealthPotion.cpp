0c0502be398447eb6a3c90e98f94cb7e88852ef1
Haroxgaming <mathis.denamur.pro@gmail.com>
2024-12-09 11:41:54 +0100

    Bug fix
    Bug fix

---
+++ ProjectCPP/Consomable/Usage unique/HealthPotion.cpp
@@ -0,0 +1,13 @@
+﻿#include "HealthPotion.h"
+
+HealthPotion::HealthPotion()
+{
+    quantite = 1;
+    name = "Health Potion";
+    effets = "Permet de soigner 10pv";
+}
+
+void HealthPotion::utiliser()
+{
+    
+}    
