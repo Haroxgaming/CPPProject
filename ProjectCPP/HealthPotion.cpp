181dcc0f5b634020f56bc0cc9c70ef13ddf9cb06
Haroxgaming <mathis.denamur.pro@gmail.com>
2024-12-09 11:39:52 +0100

    Revert "Reapply "Consomable""
    This reverts commit 91fe18392000c9e9f2ce3d1b53b8aed9fb6b772b.

---
+++ ProjectCPP/HealthPotion.cpp
@@ -0,0 +1,13 @@
+﻿#include "HealthPotion.h"
+
+void HealthPotion::utiliser()
+{
+    if (quantite > 0) {
+        std::cout << "Vous utilisez une " << name << ". Vous récupérez " 
+                  << soin << " points de vie." << std::endl;
+        quantite--;
+    } else
+    {
+        std::cout << name << " est épuisée !" << std::endl;
+    }
+}    
