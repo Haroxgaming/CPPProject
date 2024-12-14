33bc61f0bceaa8a1634ed52b689de774ff990ced
Nelson0811 <nfc.navarro@outlook.fr>
2024-12-14 12:32:52 +0100

    Inventory

---
+++ ProjectCPP/Inventory.cpp
@@ -0,0 +1,56 @@
+﻿#include "inventory.h"
+
+// ======= Item =======
+Item::Item(const std::string& name, int quantity, double price)
+    : name(name), quantity(quantity) {}
+
+std::string Item::getName() const {
+    return name;
+}
+
+int Item::getQuantity() const {
+    return quantity;
+}
+
+
+void Item::setQuantity(int qty) {
+    quantity = qty;
+}
+
+void Item::display() const {
+    std::cout << "Nom: " << name
+              << ", Quantité: " << quantity;
+              
+}
+
+// ======= Inventaire =======
+void Inventaire::addItem(const Item& item) {
+    for (auto& existingItem : items) {
+        if (existingItem.getName() == item.getName()) {
+            existingItem.setQuantity(existingItem.getQuantity() + item.getQuantity());
+            return;
+        }
+    }
+    items.push_back(item);
+}
+
+void Inventaire::removeItem(const std::string& itemName) {
+    for (auto it = items.begin(); it != items.end(); ++it) {
+        if (it->getName() == itemName) {
+            items.erase(it);
+            std::cout << "Objet supprimé : " << itemName << "\n";
+            return;
+        }
+    }
+    std::cout << "Objet non trouvé : " << itemName << "\n";
+}
+
+void Inventaire::displayInventory() const {
+    std::cout << "=== Inventaire ===\n";
+    for (const auto& item : items) {
+        item.display();
+    }
+    if (items.empty()) {
+        std::cout << "Inventaire vide.\n";
+    }
+}
---
+++ ProjectCPP/Inventory.h
@@ -0,0 +1,37 @@
+﻿#ifndef INVENTAIRE_H
+#define INVENTAIRE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Item {
+private:
+    std::string name;
+    int quantity;
+    
+public:
+    Item(const std::string& name, int quantity);
+
+   
+    std::string getName() const;
+    int getQuantity() const;
+    
+    void setQuantity(int qty);
+    
+
+    void display() const;
+};
+
+class Inventaire {
+private:
+    std::vector<Item> items;
+
+public:
+   
+    void addItem(const Item& item);
+    void removeItem(const std::string& itemName);
+    void displayInventory() const;
+};
+
+#endif 
